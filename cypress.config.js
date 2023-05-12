ecf42f508624b48b08de0ad322e5b637a556662e
Igor Khomenko <xomigrek@gmail.com>
2023-05-12 08:44:38 +0300

    Added scripts

--- cypress.config.js
+++ cypress.config.js
@@ -1,8 +1,8 @@
 const { defineConfig } = require("cypress");
 
 module.exports = defineConfig({
-  viewportWidth: 1366,
-  viewportHeight: 768,
+  viewportWidth: 390,
+  viewportHeight: 844,
   e2e: {
     baseUrl: 'http://localhost:3000/',
     setupNodeEvents(on, config) {
